d1f9dd76821840c8f505c1488a9e58c486405d30
Sakshi-Khutwad <sakshikhutwad09@gmail.com>
2025-01-26 10:38:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import './index.css'
+import solarVideo from './assets/solar_video.mp4'
+import { useTypewriter, Cursor } from 'react-simple-typewriter'
+import NavBar  from './components/NavBar'
+import Services from './components/Services'
+
+function App() {
+
+  const [typeEffect, { wordIndex }] = useTypewriter({
+    words: ['Lower bills, smaller footprint—Go Solar for a Brighter Future.',
+            'A Smart Investment Today, a lasting legacy Tomorrow.',
+            'Choose Solar—Save Money and The Planet.',
+            'Clean, Affordable Energy for Responsible Citizens.'],
+    loop: true,
+    typeSpeed: 100,
+    deleteSpeed: 40
+  })
+
+  const colors = ['text-orange-500', 'text-green-500', 'text-blue-500', 'text-yellow-500']
+  
+  return (
+    <>
+      <>
+  <div className="relative">
+    <div className="absolute top-0 left-0 w-full z-10">
+      <NavBar />
+    </div>
+
+    <video
+      src={solarVideo}
+      autoPlay
+      loop
+      muted
+      className="h-screen w-full object-cover"
+    />
+    <div className="absolute inset-0 bg-black bg-opacity-65"></div>
+
+    <div className="absolute inset-0 flex flex-col items-center justify-center z-20">
+      <div className="w-full text-center">
+        <div className="h-full">
+          <div className="flex items-center justify-center">
+            <h1 className="text-3xl md:text-7xl bg-clip-text m-3 text-transparent bg-gradient-to-r from-orange-700 to-yellow-700 font-extrabold">
+              Why Solar? <br />
+            </h1>
+          </div>
+          <div className="flex items-center justify-center">
+            <h1 className="h-full text-3xl md:text-6xl bg-clip-text m-3 text-transparent bg-gradient-to-r from-yellow-600 to-green-700 font-extrabold">
+              {typeEffect}
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <section className="flex justify-center bg-black">
+    <Services />
+  </section>
+</>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import { Link } from 'react-scroll';
+import { FaTimes } from 'react-icons/fa';
+import { CiMenuFries } from 'react-icons/ci';
+import logo from '../assets/logo_cropped.png'
+
+function NavBar() {
+    const [isOpen, setIsOpen] = useState(false);
+    const handleClick = () => setIsOpen(!isOpen);
+
+    const content = (
+        <div className="md:hidden bg-black absolute top-20 left-0 w-full z-10">
+            <ul className="flex flex-col items-center">
+                <Link to="Home" smooth={true}>
+                    <li className="hover:text-orange-700 text-slate-50 text-xl cursor-pointer font-normal w-full text-center py-2">
+                        Home
+                    </li>
+                </Link>
+                <Link to="Services" smooth={true}>
+                    <li className="hover:text-orange-700 text-slate-50 text-xl cursor-pointer font-normal w-full text-center py-2">
+                        Our Services
+                    </li>
+                </Link>
+                <Link to="About" smooth={true}>
+                    <li className="hover:text-orange-700 text-slate-50 text-xl cursor-pointer font-normal w-full text-center py-2">
+                        About Us
+                    </li>
+                </Link>
+                <Link to="Contact" smooth={true}>
+                    <li className="hover:text-orange-700 text-slate-50 text-xl cursor-pointer font-normal w-full text-center py-2">
+                        Contact
+                    </li>
+                </Link>
+            </ul>
+        </div>
+    );
+
+    return (
+        <nav>
+
+            <div className="flex my-4">
+
+                <div className="flex flex-1 h-8 mx-6 md:h-14">
+                    <img src={logo} alt="Aditya Solar Logo" className="" />
+                </div>
+
+                <div className="hidden md:flex mx-6 my-2">
+
+                    <div className="flex md:space-x-16">
+
+                        <div>
+                            <Link to="Home" smooth={true}>
+                                <span className="transition duration-700 ease-in-out border-b border-transparent hover:border-orange-700 hover:text-orange-700 text-slate-50 text-xl cursor-pointer font-normal">
+                                    Home
+                                </span>
+                            </Link>
+                        </div>
+
+                        <div>
+                            <Link to="Services" smooth={true}>
+                                <span className="transition duration-700 ease-in-out border-b border-transparent hover:border-orange-700 hover:text-orange-700 text-slate-50 text-xl cursor-pointer font-normal">
+                                    Our Services
+                                </span>
+                            </Link>
+                        </div>
+
+                        <div>
+                            <Link to="About" smooth={true}>
+                                <span className="transition duration-700 ease-in-out border-b border-transparent hover:border-orange-700 hover:text-orange-700 text-slate-50 text-xl cursor-pointer font-normal">
+                                    About Us
+                                </span>
+                            </Link>
+                        </div>
+                        <div>
+                            <Link to="Contact Us" smooth={true}>
+                                <span className="transition duration-700 ease-in-out border-b border-transparent hover:border-orange-700 hover:text-orange-700 text-slate-50 text-xl cursor-pointer font-normal">
+                                    Contact
+                                </span>
+                            </Link>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className="md:hidden mx-4">
+                    <button onClick={handleClick} className="text-white">
+                        {isOpen ? <FaTimes /> : <CiMenuFries />}
+                        {isOpen && content}
+                    </button>
+                </div>
+            </div>
+        </nav >
+    );
+}
+
+export default NavBar
