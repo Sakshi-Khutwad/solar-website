f03ff7de0827687772f7dbde7b3cf6e950e266ce
Sakshi Vijay Khutwad <sakshi@Sakshis-MacBook-Air.local>
2026-06-09 22:30:51 +0530

    change about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,6 +4,7 @@ function About(){
     return (
         <>
             <div className="bg-orange-300 text-7xl ">This is About Us page and this is information.</div>
+            <div className="bg-orange-300 text-7xl ">About Us Page</div>
         </>
     )
 }
